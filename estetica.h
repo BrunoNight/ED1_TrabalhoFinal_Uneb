10b9ed36ec68fe8d2e078a081411d7f9f2f31fa3
BrunoNight <brunobrrt3680@gmail.com>
2026-06-28 15:17:52 -0300

    Adicionado biblioteca "estetica"
    Implementado no sistema "estetica.h" e "estetica.cpp" para: bordas simples, centralização de texto e cabeçalho com nome do sistema

---
+++ estetica.h
@@ -0,0 +1,24 @@
+// 1. Inicialização da inclusão do header
+#ifndef ESTETICA_H_INCLUDED
+#define ESTETICA_H_INCLUDED
+
+// 2. Incluir bibliotecas existentes
+#include <string>
+
+// 3. Função para cores de texto do sistema
+namespace Estetica {
+    const std::string RED = "\033[1;31m";
+    const std::string YELLOW = "\033[1;33m";
+    const std::string GREEN = "\033[1;32m";
+    const std::string RESET = "\033[0m";
+}
+
+// 4. Outras funções para o estetica.cpp
+// // 4.1. EXTRAIR LARGURA DO TERMINAL
+int getLarguraTerminal();
+// // 4.2. CENTRALIZAR TEXTO
+void centralizarTexto(const string& texto);
+// // 4.3. CABEÇALHO DO SISTEMA
+void esteticaCabeçalhoSistema();
+
+#endif // ESTETICA_H_INCLUDED
