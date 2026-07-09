6170fe9b33aea2a47ab52f9af34b2c4f8c4055a4
BrunoNight <brunobrrt3680@gmail.com>
2026-07-09 16:35:14 -0300

    Ajustes_09/07/2026
    Feito as seguintes correções:
    1-- Acrescentado "#include "conteudo.h"
    2-- Ajustado chamada das classes das listas (seus nomes)
    3-- Acrescentado à classe NodoA as variáveis correspondentes à filtragem de conteúdo
    4-- Ajustado nomenclatura dos tipos de lista na função "inserirFiltrar"

--- arvore.h
+++ arvore.h
@@ -5,28 +5,34 @@
 // 2. Inclusão de bibliotecas existentes
 #include <string>
 
-// 3. Declaração de classes e structs das bibliotecas criadas para evitar inclusão cíclica
-class ListaContCad; // CONSERTAR
-class ListaContRec; // CONSERTAR
+// 3. Inclusão de bibliotecas criadas
+#include "conteudo.h"
 
-// 4. Classe do nó da árvore
+// 4. Declaração de classes e structs das bibliotecas criadas para evitar inclusão cíclica
+class ListaDupla;
+class ListaSimples;
+
+// 5. Classe do nó da árvore
 class NodoA { public:
     std::string texto;
     NodoA* sim; // Para a esquerda
     NodoA* nao; // Para a direita
     bool folha; // Identificar se é o última nível ou não
     NodoA() : texto(""), sim(nullptr), nao(nullptr), folha(false) {} // Construtor para inicializar de forma segura os ponteiros sem risco de lixo de memória
+    TipoConteudo tipoFiltro;
+    GeneroConteudo generoFiltro;
+    FiltroCronologico cronologiaFiltro;
 };
 
-// 5. Classe da árvore
+// 6. Classe da árvore
 class Arvore { public:
     NodoA* raiz;
     Arvore(); // Construtor da árvore
     ~Arvore(); // Destrutor da árvore
-    void apagarArvore(NodoA *atual);
+    void apagarArvore(NodoA* atual);
     void inserirPorCaminho(const std::string& caminho, const std::string& filtro, bool folha);
     void gerarArvoreDecisao();
-    void inserirFiltrar(NodoA* atual, ListaContCad& listaCad, ListaContRec& listaRec); // CONSERTAR
+    void inserirFiltrar(NodoA* atual, ListaDupla& listaCad, ListaSimples& listaRec);
 };
 
 #endif // ARVORE_H_INCLUDED
